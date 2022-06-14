f3d286936de87703a40b63795506442ae5512cf7
Matias <efbingenieria@gmail.com>
2022-06-14 10:31:27 -0300

    Creamos en index todo para poder utilizar la base de datos

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('./mongo')
+
+
 const express = require('express')    //Importar el modulo http utilizando Common.JS
 const app = express()
 const cors = require('cors')
-
-
+const Note = require('./models/Note')
 
 const logger = require('./loggerMiddleware')
 
@@ -11,26 +13,7 @@ app.use(express.json())
 
 app.use(logger)
 
-let notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        date: '2019-05-30T17:30:31.098Z',
-        important: true,
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only JavaScript',
-        date: '2019-05-30T18:39:34.091Z',
-        important: false,
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        date: '2019-05-30T19:20:14.298Z',
-        important: true,
-    },
-]
+let notes = []
 
 // const app = http.createServer((request, response) => {      //Callback, funcion que se ejecuta cada vez que
 //     response.writeHead(200, { 'Content-Type':'application/json'}) //le llegue un request (petición al servidor)
