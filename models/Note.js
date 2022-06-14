20c17c643ea96a8ddebe0d4ccac2d410fe1707e9
Matias <efbingenieria@gmail.com>
2022-06-14 11:10:26 -0300

    Realizamos el proceso para que el objeto guardado de la transformacion de .json sea como quiera

--- models/Note.js
+++ models/Note.js
@@ -7,6 +7,16 @@ const noteSchema = new Schema({
     important: Boolean
 })
 
+//Lo que hacemos acá es decir al Schema de nota, como quiero que .json haga la transmormacion del objeto
+// Si no especifico esto, response.json(notes) devuelve un objeto muy conplejo y con _id y __v q no sirve
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id  // agrego al objeto el id sin el guion bajo al lado
+        delete returnedObject._id               // elimino los campos que no quiero. Recordemos que 
+        delete returnedObject.__v               // estoy borrando la trnasformación a JSON y no los datos
+    }                                           // crudos de la BD, por eso puedo utilizar delete
+})
+
 //construimos un modelo
 const Note = model('Note', noteSchema)
 
